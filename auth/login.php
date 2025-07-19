3420bca86fc099df38a7aa3105c8197c351bae4a
Manjusg24 <150472115+Manjusg24@users.noreply.github.com>
2025-07-19 23:10:27 +0530

    fix(auth): set session error messages on login

--- auth/login.php
+++ auth/login.php
@@ -3,31 +3,34 @@ session_start();
 
 include '../includes/db.php';
 
-if(isset($_POST["submit"])){
+if(isset($_POST["submit"])) {
     $name=$_POST["uname"];
     $pass=$_POST["pass"];
 
     $sql = "Select * from `users` where Username='$name'"; 
     $query = mysqli_query($conn,$sql);
     $count = mysqli_num_rows($query); 
-    if($count)
-    {
+    
+    if($count) {
         $fetch = mysqli_fetch_assoc($query);
         $fpass = $fetch['Password'];
         
-        if(password_verify($pass,$fpass)){
+        if(password_verify($pass,$fpass)) {
             $_SESSION['name'] = $fetch['Username'];
             echo "<script> location.replace('dashboard.php'); </script>";
+        } else {
+            $_SESSION['error'] = "&#x274c Password incorrect.";
+            header("Location: ../index.php");
+            exit();
         }
-        else{
-            echo "Password Incorrect";
-        }
-
+    } else {
+        $_SESSION['error'] = "&#x274c Invalid username.";
+        header("Location: ../index.php");
+        exit();
     }
-    else{
-    echo "Invalid Username";
-    
-    }
+} else {
+    header("Location: ../index.php");
+    exit();
 }
 
 ?>
